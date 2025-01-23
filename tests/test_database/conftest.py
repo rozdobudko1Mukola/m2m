12c78c75de5fee196e0ea692823f77d67b1bff5b
rozdobudko1Mukola <rozdobudko.nikolay@gmail.com>
2025-01-23 15:00:44 +0200

    all test pass

--- tests/test_database/conftest.py
+++ tests/test_database/conftest.py
@@ -1,28 +1,23 @@
 import pytest
-from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, Browser, Page
 from pages.login import LoginPage
 
 STAGE_USER_FREE_BILL_PLAN = "x1pt4sqawm@vvatxiy.com"
 LOCALE = 'uk-UA'
 TIMEZONE_ID = 'Europe/Kiev'
 
+
 @pytest.fixture(scope="function")
-def login_free_paln_user(base_url):
-    """Fixture for logging in a user with browser initialization."""
-    with sync_playwright() as p:
-        # Запускаємо браузер із slow_mo
-        browser = p.chromium.launch(slow_mo=500)
-        context = browser.new_context(locale=LOCALE, timezone_id=TIMEZONE_ID, base_url=base_url)
-        page = context.new_page()
-        
-        # Логін користувача
-        login_page = LoginPage(page)
-        login_page.login(STAGE_USER_FREE_BILL_PLAN, STAGE_USER_FREE_BILL_PLAN)
-        login_page.accept_btn.click()
+def login_free_paln_user(browser: Browser, base_url):
+    """Fixture for logging in a user."""
+    context = browser.new_context(locale=LOCALE, timezone_id=TIMEZONE_ID, base_url=base_url)
+    page = context.new_page()
+    login_page = LoginPage(page)
+    login_page.login(STAGE_USER_FREE_BILL_PLAN, STAGE_USER_FREE_BILL_PLAN)
+    login_page.accept_btn.click()
+    page.wait_for_timeout(1000)
 
-        yield page
+    yield page
 
-        # Закриваємо сторінку, контекст та браузер
-        page.close()
-        context.close()
-        browser.close()
+    page.close()
+    context.close() 
