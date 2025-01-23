bdd6a124ddbc0b4c6dd90fb6c87e35e0273538c7
rozdobudko1Mukola <rozdobudko.nikolay@gmail.com>
2025-01-23 10:26:25 +0200

    fix run test with headed mode

--- tests/test_database/conftest.py
+++ tests/test_database/conftest.py
@@ -1,6 +1,8 @@
 import pytest
 from playwright.sync_api import Browser, Page
 from pages.login import LoginPage
+from playwright.sync_api import sync_playwright
+
 
 STAGE_USER_FREE_BILL_PLAN = "x1pt4sqawm@vvatxiy.com"
 LOCALE = 'uk-UA'
@@ -12,8 +14,18 @@ def close_context(context, page):
     page.close()
     context.close()
 
+
 @pytest.fixture(scope="function")
-def login_user(browser: Browser, base_url):
+def browser():
+    """Запуск браузера з slow_mo."""
+    with sync_playwright() as p:
+        browser = p.chromium.launch(slow_mo=500)  # Уповільнення на 500 мс
+        yield browser
+        browser.close()
+
+
+@pytest.fixture(scope="function")
+def login_user(browser, base_url):
     """Fixture for logging in a user."""
     context = browser.new_context(locale=LOCALE, timezone_id=TIMEZONE_ID, base_url=base_url)
     page = context.new_page()
